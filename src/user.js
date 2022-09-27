e2f9b22fa15e944cb73aa5b1eb8b8e5c20b71b85
Reja <rezanurrochmat@gmail.com>
2022-09-27 10:40:16 +0700

    intial commit

---
+++ src/user.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.user = void 0;
+function user() {
+    return "Hello TypeScript";
+}
+exports.user = user;
+console.log(user());
---
+++ src/user.ts
@@ -0,0 +1,5 @@
+export function user() {
+    return `Hello TypeScript`
+}
+
+console.log(user())
